90e2b975e5994c8154d0f28b2090019eebfca47a
Smileyx96 <jasonleezhiyong@gmail.com>
2020-04-15 23:51:33 +0800

    move web folder to another repo

--- web/src/components/index.js
+++
@@ -1,7 +0,0 @@
-import LandingPage from './LandingPage';
-import SearchBar from './SearchBar';
-import AboutPage from './AboutPage';
-import ResultPage from './ResultPage';
-
-
-export { LandingPage, SearchBar, AboutPage, ResultPage };
